367e506c435ce282250e479e8de8e99190fabd7b
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2022-12-13 23:47:40 +0500

    created site logo for theme

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const link =document.querySelector(".header-nav-link");
-const body =document.querySelector(".body")
+const body =document.querySelector(".body");
+const logoDark =document.querySelector(".header-logo-black")
  link.addEventListener("click" ,function(evt){
     evt.preventDefault();
-    body.classList.toggle("dark-body")
+    body.classList.toggle("dark-body");
+    logoDark.classList.toggle("logo-none");
  })
