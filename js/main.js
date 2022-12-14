615ead4096a90d8eee79027c48bad8bc21bbba55
sherzodxon <yarmatxonovsherzodxon@gmail.com>
2022-12-14 16:16:07 +0500

    remoted with pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-const link =document.querySelector(".header-nav-link");
-const body =document.querySelector(".body");
-const logoDark =document.querySelector(".header-logo-black")
- link.addEventListener("click" ,function(evt){
-    evt.preventDefault();
-    body.classList.toggle("dark-body");
-    logoDark.classList.toggle("logo-none");
- })
+const link = document.querySelector(".header-nav-link");
+const body = document.querySelector(".body");
+const logoDark = document.querySelector(".header-logo-black");
+const footerLogoDark = document.querySelector(".footer-logo-dark");
+const footerLogoLight = document.querySelector(".footer-logo");
+link.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   body.classList.toggle("dark-body");
+   logoDark.classList.toggle("logo-none");
+   footerLogoDark.classList.toggle("footer-logo-block");
+   footerLogoLight.classList.toggle("logo-none")
+})
